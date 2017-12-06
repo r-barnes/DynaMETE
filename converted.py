415f2cab8ab4b19b3a7cc517f38a2c1fd01b801d
jamesli1997 <jamesli1997@gmail.com>
2017-12-06 12:50:59 -0800

    added code for checking normalization and calculating averages.

--- converted.py
+++ converted.py
@@ -64,6 +64,13 @@ f[0,5] = 1 #100% probability of having 10 species at t=0
 G[0,5] = 1 #100% probability of having 10 individuals at t=0
 H[0,5] = 1 #100% probability of having 10 units of metabolic rate (Watts) at t=0
 
+sum_H = np.array([0 for x in range(MAX_TIMESTEP)]) #Use this later to check normalization
+sum_G = np.array([0 for x in range(MAX_TIMESTEP)]) #Use this later to check normalization
+sum_F = np.array([0 for x in range(MAX_TIMESTEP)]) #Use this later to check normalization
+
+E_avg = np.array([0 for x in range(MAX_TIMESTEP)]) #Use this later to calculate average
+N_avg = np.array([0 for x in range(MAX_TIMESTEP)]) #Use this later to calculate average
+S_avg = np.array([0 for x in range(MAX_TIMESTEP)]) #Use this later to calculate average
 
 
 ######################
@@ -203,3 +210,105 @@ for t in range(1,MAX_TIMESTEP):
     - f[t-1,-1]*Svalue[-1]**(4/3)*(d0*expected_E1*expected_N2 + d1*expected_E2*expected_N2)
     + lam0*Svalue[-2]*f[t-1,-2]
   )
+
+  ####################
+  #Check Normalization
+  ####################
+
+  #see wether the probabilities sum up to 1
+  
+  prob_E = np.sum(H[t, 1:])
+  sum_H[t] = prob_E
+
+  prob_N = np.sum(G[t, 1:])
+  sum_G[t] = prob_N
+
+  prob_S = np.sum(f[t, 1:])
+  sum_F[t] = prob_S
+
+  #Calculate <E>, <N>, <S>
+  avg_E = 0
+  avg_N = 0
+  avg_S = 0
+
+  #Offsets from each column to its left and right neighbours
+  ci = list(range(2,MAX_METABOLIC-1)) #Center index
+  li = np.roll(ci,shift= 1)           #Left index
+  ri = np.roll(ci,shift=-1)           #Right index
+
+  avg_E = avg_E + Evalue[ci] * H[t, ci]
+
+  #Offsets from each column to its left and right neighbours
+  ci = list(range(2,MAX_INDIVIDUALS-1)) #Center index
+  li = np.roll(ci,shift= 1)           #Left index
+  ri = np.roll(ci,shift=-1)           #Right index
+
+  avg_N = avg_N + Nvalue[ci] * G[t, ci]
+
+  #Offsets from each column to its left and right neighbours
+  ci = list(range(2,MAX_SPECIES-1)) #Center index
+  li = np.roll(ci,shift= 1)           #Left index
+  ri = np.roll(ci,shift=-1)           #Right index
+
+  avg_S = avg_S + Svalue[ci] * f[t, ci]
+
+
+  #Code from R
+  for (t in 2:nrow(H)){
+    prob_E = 0
+    for (E in 1:(ncol(H))){
+      prob_E = prob_E + H[t,E]
+    }
+    sum_H[t-1] = prob_E
+  }
+
+  sum_f <- vector(,length = 50001)
+  for (t in 2:nrow(f)){
+    prob_S = 0
+    for (S in 1:(ncol(f))){
+      prob_S = prob_S + f[t,S]
+    }
+    sum_f[t-1] = prob_S
+  }
+
+  sum_G <- vector(,length = 50001)
+  for (t in 2:nrow(G)){
+    prob_N = 0
+    for (N in 1:(ncol(G))){
+      prob_N = prob_N + G[t,N]
+    }
+    sum_G[t-1] = prob_N
+  }
+
+  #Calculate <S>, <N> and <E>
+  S_avg <- vector(,length = 50000)
+  N_avg <- vector(,length = 50000)
+  E_avg <- vector(,length = 50000)
+
+  for (t in 2:50001){
+    avg_S = 0
+    avg_N = 0
+    avg_E = 0
+    for (N in 1:ncol(G)){
+      avg_N = avg_N + G[1,N]*G[t,N]
+      N_avg[t-1] = avg_N
+    }
+    
+    for (S in 1:ncol(f)){
+      avg_S = avg_S + f[1,S]*G[t,S]
+      S_avg[t-1] = avg_S
+    }
+    
+    for(E in 1:ncol(H)){
+      avg_E = avg_E + H[1,E]*H[t,E]
+      E_avg[t-1] = avg_E
+    }
+  } 
+
+
+
+
+
+
+
+
