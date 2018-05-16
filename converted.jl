31cb12e117a12f85260b5a13d425ff6583acacde
Richard Barnes <rbarnes@umn.edu>
2018-05-16 13:42:59 -0600

    Working

--- converted.jl
+++ converted.jl
@@ -1,111 +1,10 @@
 #!/usr/bin/env julia
 
-######################
-#PACKAGE CONFIGURATION
-######################
-
-
-######################
-#CONSTANTS
-######################
-b0    = 0.0025       #Birth rate per capita
-d0    = 0.001        #Death rate per capita
-lam0  = 0            #Speciation rate
-m     = 0.002        #Migration rate
-Emax  = 300000       #Soft maximum for energy (TODO)
-EEmax = 1            #EEmax = 1:E/Emax
-                     #Ontogenic growth dMetabolicRate/dt=w0*e^(2/3)-w1*e (TODO)
-w0    = 0.01         #Ontogenic growth (growth of individual over its lifespan): See above
-w1    = 0.0003       #Ontogenic growth (growth of individual over its lifespan): See above
-Smeta = 60           #Species richness of the meta community
-d1    = (b0-d0)/Emax #Density dependent contribution to death rate
-meta  = 100          #1000/(E/N) of meta
-
-MAX_TIMESTEP    = 5000 #50001
-
-MAX_INDIVIDUALS = 100 #251
-MAX_SPECIES     = 65 # 65
-MAX_METABOLIC   = 100 #324
+import PyPlot
 
 
 
-######################
-#GLOBALS
-######################
-
-#Arrays are arranged such that they are MAX_TIMESTEP high and (e.g.) MAX_SPECIES
-#wide
-
-# f = zeros((MAX_TIMESTEP, MAX_SPECIES))     #Total number of species
-# G = zeros((MAX_TIMESTEP, MAX_INDIVIDUALS)) #Total number of individuals
-# H = zeros((MAX_TIMESTEP, MAX_METABOLIC))   #Total metabolic rate
-
-fprev = zeros(MAX_SPECIES)
-Gprev = zeros(MAX_INDIVIDUALS)
-Hprev = zeros(MAX_METABOLIC)
-
-f = zeros(MAX_SPECIES)
-G = zeros(MAX_INDIVIDUALS)
-H = zeros(MAX_METABOLIC)
-
-
-######################
-#INITIAL CONDITIONS
-######################
-
-#NOTE: The equations below assume that Nvalue[0] = 0 and Evalue[0] = 0
-#TODO: Try to eliminate Nint and Eint
-Nint   = 10.
-Eint   = 1000.
-Sint   = 1.
-Nvalue = Nint*collect(0:MAX_INDIVIDUALS-1)
-Evalue = Eint*collect(0:MAX_METABOLIC  -1)
-Svalue = Sint*collect(0:MAX_SPECIES    -1)
-
-fprev[6] = 1 #100% probability of having 10 species at t=1
-Gprev[6] = 1 #100% probability of having 10 individuals at t=1
-Hprev[6] = 1 #100% probability of having 10 units of metabolic rate (Watts) at t=1
-
-sum_H = zeros(MAX_TIMESTEP) #Use this later to check normalization
-sum_G = zeros(MAX_TIMESTEP) #Use this later to check normalization
-sum_F = zeros(MAX_TIMESTEP) #Use this later to check normalization
-
-avg_E = zeros(MAX_TIMESTEP) #Use this later to calculate average
-avg_N = zeros(MAX_TIMESTEP) #Use this later to calculate average
-avg_S = zeros(MAX_TIMESTEP) #Use this later to calculate average
-
-
-######################
-#TIME LOOP
-######################
-
-t=2
-# for t = 2:MAX_TIMESTEP
-  ###########################
-  #SET UP INTERMEDIATE VALUES
-  ###########################
-
-  #NOTE: This blows up if Nvalue[0]=0, so we only look at Nvalue[1:]
-  logN        = log.(Nvalue[2:end])
-  expected_N1 = sum(Nvalue[2:end].^(1/3)                .*Gprev[2:end]) #expected_N1 = <N^(1/3)>
-  expected_N2 = sum(1./logN                             .*Gprev[2:end]) #expected_N2 = <1/ln(N)>
-  expected_N3 = sum(1./logN.^(1/3)                      .*Gprev[2:end]) #expected_N3 = <1/ln(N)^(1/3)>
-  expected_N4 = sum(logN.^(1/3)                         .*Gprev[2:end]) #expected_N4 = <ln(N)^(1/3)>
-  expected_N5 = sum(Nvalue[2:end].^(1/3) ./ logN.^(2/3) .*Gprev[2:end]) #expected_N5 = <N^(1/3)/ln(N)^(2/3)
-
-  #NOTE: This blows up if Evalue[0]=0, so we only look at Evalue[1:]
-  expected_E1 = sum(Evalue[2:end].^(-1/3) .* Hprev[2:end])   #avg_E1 = <E^(-1/3)>
-  expected_E2 = sum(Evalue[2:end].^( 2/3) .* Hprev[2:end])   #avg_E2 = <E^(2/3)>
-
-  ###################
-  #Calculate H matrix
-  ###################
-
-  #Offsets from each column to its left and right neighbours
-  #2:end-1 = list(range(2,MAX_METABOLIC-1)) #Center index
-  #1:end-2 = np.roll(2:end-1,shift= 1)           #Left index
-  #2: = np.roll(2:end-1,shift=-1)           #Right index
-
+function UpdateH!(H,Hprev,expected_N5)
   #Our strategy is to perform our calculations as though all of the columns are
   #general cases. This results in incorrect values of the lestmost and rightmost
   #columns. We will fix these immediately following.
@@ -147,24 +46,21 @@ t=2
     - (d0*Evalue[end  ].^(2/3) + d1*Evalue[end].^(5/3)) * expected_N5*Hprev[end  ]/Eint
   )
 
-  ###################
-  #Calculate G matrix
-  ###################
+  return nothing
+end
 
-  #Offsets from each column to its left and right neighbours
-  #2:end-1 = list(range(2,MAX_INDIVIDUALS-1)) #Center index
-  #1:end-2 = np.roll(2:end-1,shift= 1)           #Left index
-  #2: = np.roll(2:end-1,shift=-1)           #Right index
 
+
+function UpdateG!(G,Gprev,expected_E1,expected_E2)
   #Our strategy is to perform our calculations as though all of the columns are
   #general cases. This results in incorrect values of the lestmost and rightmost
   #columns. We will fix these immediately following.
-  G[2:end-1] = (
-      Gprev[2:end-1] 
-    - m*(Gprev[2:end-1] - Gprev[1:end-2])/Nint
-    + Gprev[1:end-2].*(     b0*expected_E1                 ) .* Nvalue[1:end-2].^(4/3).*log.(Nvalue[1:end-2]).^(1/3)/Nint
-    - Gprev[2:end-1].*((b0+d0)*expected_E1 + d1*expected_E2) .* Nvalue[2:end-1].^(4/3).*log.(Nvalue[2:end-1]).^(1/3)/Nint
-    + Gprev[3:end  ].*(     d0*expected_E1 + d1*expected_E2) .* Nvalue[3:end  ].^(4/3).*log.(Nvalue[3:end  ]).^(1/3)/Nint
+  G[3:end-1] = (
+      Gprev[3:end-1] 
+    - m*(Gprev[3:end-1] - Gprev[2:end-2])/Nint
+    + Gprev[2:end-2].*(     b0*expected_E1                 ) .* Nvalue[2:end-2].^(4/3).*log.(Nvalue[2:end-2]).^(1/3)/Nint
+    - Gprev[3:end-1].*((b0+d0)*expected_E1 + d1*expected_E2) .* Nvalue[3:end-1].^(4/3).*log.(Nvalue[3:end-1]).^(1/3)/Nint
+    + Gprev[4:end  ].*(     d0*expected_E1 + d1*expected_E2) .* Nvalue[4:end  ].^(4/3).*log.(Nvalue[4:end  ]).^(1/3)/Nint
   )
 
   #######################
@@ -188,16 +84,12 @@ t=2
     + Gprev[end-1].*(b0*expected_E1)                  .*log.(Nvalue[end-1]).^(1/3).*Nvalue[end-1].^(4/3)/Nint
     - Gprev[end  ].*(d0*expected_E1 + d1*expected_E2) .*log.(Nvalue[end  ]).^(1/3).*Nvalue[end  ].^(4/3)/Nint
   )
+  return nothing
+end
 
-  ###################
-  #Calculate F matrix
-  ###################
 
-  #Offsets from each column to its left and right neighbours
-  #2:end-1 = list(range(2,MAX_SPECIES-1))  #Center index
-  #1:end-2 = np.roll(2:end-1,shift= 1)          #Left index
-  #2: = np.roll(2:end-1,shift=-1)          #Right index
 
+function UpdateF!(f,fprev,expected_N2,expected_E1,expected_E2)
   #Our strategy is to perform our calculations as though all of the columns are
   #general cases. This results in incorrect values of the lestmost and rightmost
   #columns. We will fix these immediately followingself.
@@ -224,49 +116,181 @@ t=2
     + lam0*Svalue[end-1]*fprev[end-1]
   )
 
-  ####################
-  #Check Normalization
-  ####################
+  return nothing
+end
 
-  #see wether the probabilities sum up to 1
-  sum_H[t] = sum(H[ 2:end]) #the sum of the probabilities of Energy at time t
-  sum_G[t] = sum(G[ 2:end]) #the sum of the probabilities of Individuals at time t
-  sum_F[t] = sum(f[ 2:end]) #the sum of the probabilities of Species at time t
 
-  #Calculate <E>, <N>, <S>
-  avg_E[t] = sum(Evalue[3:end] .* H[ 3:end])
-  avg_N[t] = sum(Nvalue[3:end] .* G[ 3:end])
-  avg_S[t] = sum(Svalue[3:end] .* f[ 3:end])
-# end
 
-fig, ax = plt.subplots(1,3, sharex=True, sharey=True)
-ax[0].plot(sum_H, label="sum_H")
-ax[0].set_ylim([-0.1,1.1])
-ax[1].plot(sum_G, label="sum_G")
-ax[2].plot(sum_F, label="sum_F")
-fig.suptitle("Normalization Check (should be 1)")
-plt.legend()
-plt.show()
 
-fig, ax = plt.subplots(1,3, sharex=True)
-ax[0].plot(avg_S, label="avg_S")
-ax[1].plot(avg_N, label="avg_N")
-ax[2].plot(avg_S, label="avg_S")
-fig.suptitle("Average of state variables")
-plt.legend()
-plt.show()
 
-fig, ax = plt.subplots(1,3, sharex=True)
-ax[0].plot(Svalue,f[-1,:], label="Svalue")
-ax[1].plot(Nvalue,G[-1,:], label="Nvalue")
-ax[2].plot(Evalue,H[-1,:], label="Evalue")
-fig.suptitle("State variable distribution at final time step")
-plt.legend()
-plt.show()
+######################
+#CONSTANTS
+######################
+const b0              = 0.0025       #Birth rate per capita
+const d0              = 0.001        #Death rate per capita
+const lam0            = 0            #Speciation rate
+const m               = 0.001        #Migration rate
+const Emax            = 300000       #Soft maximum for energy (TODO)
+const w0              = 0.01         #Ontogenic growth (growth of individual over its lifespan): See above
+const w1              = 0.0003       #Ontogenic growth (growth of individual over its lifespan): See above
+const Smeta           = 60           #Species richness of the meta community
+const d1              = (b0-d0)/Emax #Density dependent contribution to death rate
+const meta            = 100          #1000/(E/N) of meta
+const Nint            = 10.          #Number of individuals per individual bin
+const Eint            = 100.         #Number of energy units per energy bin
+const Sint            = 1.           #Number of species per species bin
+const MAX_TIMESTEP    = 50001        #Number of timesteps to take 
+const MAX_INDIVIDUALS = 251          #Number of bins for individuals
+const MAX_SPECIES     = 65           #Number of bins for species
+const MAX_METABOLIC   = 3240         #Number of bins for energy
 
-# lp = LineProfiler()
-# lp_wrapper = lp(main)
-# lp_wrapper()
-# lp.print_stats()
+const Nvalue = Nint*collect(0:MAX_INDIVIDUALS-1)
+const Evalue = Eint*collect(0:MAX_METABOLIC  -1)
+const Svalue = Sint*collect(0:MAX_SPECIES    -1)
 
-#main()
+
+
+######################
+#GLOBALS
+######################
+
+function main()
+  fprev = zeros(MAX_SPECIES)
+  Gprev = zeros(MAX_INDIVIDUALS)
+  Hprev = zeros(MAX_METABOLIC)
+
+  f = zeros(MAX_SPECIES)
+  G = zeros(MAX_INDIVIDUALS)
+  H = zeros(MAX_METABOLIC)
+
+  ######################
+  #INITIAL CONDITIONS
+  ######################
+
+  fprev[6]   = 1 #100% probability of having 10 species at t=1
+  Gprev[6]   = 1 #100% probability of having 10 individuals at t=1
+  Hprev[101] = 1 #100% probability of having 10 units of metabolic rate (Watts) at t=1
+
+  sum_H = zeros(MAX_TIMESTEP) #Use this later to check normalization
+  sum_G = zeros(MAX_TIMESTEP) #Use this later to check normalization
+  sum_F = zeros(MAX_TIMESTEP) #Use this later to check normalization
+
+  avg_E = zeros(MAX_TIMESTEP) #Use this later to calculate average
+  avg_N = zeros(MAX_TIMESTEP) #Use this later to calculate average
+  avg_S = zeros(MAX_TIMESTEP) #Use this later to calculate average
+
+
+  ######################
+  #TIME LOOP
+  ######################
+
+  for t = 2:MAX_TIMESTEP
+    if t%100==0
+      println("t = $t")
+    end
+    ###########################
+    #SET UP INTERMEDIATE VALUES
+    ###########################
+
+    #NOTE: This blows up if Nvalue[0]=0, so we only look at Nvalue[1:]
+    logN        = log.(Nvalue[2:end])
+    expected_N1 = sum(Nvalue[2:end].^(1/3)                .*Gprev[2:end]) #expected_N1 = <N^(1/3)>
+    expected_N2 = sum(1./logN                             .*Gprev[2:end]) #expected_N2 = <1/ln(N)>
+    expected_N3 = sum(1./logN.^(1/3)                      .*Gprev[2:end]) #expected_N3 = <1/ln(N)^(1/3)>
+    expected_N4 = sum(logN.^(1/3)                         .*Gprev[2:end]) #expected_N4 = <ln(N)^(1/3)>
+    expected_N5 = sum(Nvalue[2:end].^(1/3) ./ logN.^(2/3) .*Gprev[2:end]) #expected_N5 = <N^(1/3)/ln(N)^(2/3)
+
+    #NOTE: This blows up if Evalue[0]=0, so we only look at Evalue[1:]
+    expected_E1 = sum(Evalue[2:end].^(-1/3) .* Hprev[2:end])   #avg_E1 = <E^(-1/3)>
+    expected_E2 = sum(Evalue[2:end].^( 2/3) .* Hprev[2:end])   #avg_E2 = <E^(2/3)>
+
+    ###################
+    #Calculate H matrix
+    ###################
+    UpdateH!(H,Hprev,expected_N5)
+
+
+    ###################
+    #Calculate G matrix
+    ###################
+
+    UpdateG!(G,Gprev,expected_E1,expected_E2)
+
+    ###################
+    #Calculate F matrix
+    ###################
+
+    UpdateF!(f,fprev,expected_N2,expected_E1,expected_E2)
+
+    f = max.(f,0)
+    G = max.(G,0)
+    H = max.(H,0)
+
+    ####################
+    #Check Normalization
+    ####################
+
+    #see wether the probabilities sum up to 1
+    sum_H[t] = sum(H[ 2:end]) #the sum of the probabilities of Energy at time t
+    sum_G[t] = sum(G[ 2:end]) #the sum of the probabilities of Individuals at time t
+    sum_F[t] = sum(f[ 2:end]) #the sum of the probabilities of Species at time t
+
+    #Calculate <E>, <N>, <S>
+    avg_E[t] = sum(Evalue[3:end] .* H[ 3:end])
+    avg_N[t] = sum(Nvalue[3:end] .* G[ 3:end])
+    avg_S[t] = sum(Svalue[3:end] .* f[ 3:end])
+
+    fprev = f
+    Gprev = G
+    Hprev = H
+  end
+
+  return sum_H, sum_G, sum_F, avg_S, avg_N, avg_E, f, G, H
+end
+
+sum_H, sum_G, sum_F, avg_S, avg_N, avg_E, f, G, H = main()
+
+
+
+
+
+
+PyPlot.subplot(311)
+PyPlot.plot(sum_H, label="sum_H")
+PyPlot.legend()
+PyPlot.ylim(-0.1,1.1)
+PyPlot.subplot(312)
+PyPlot.plot(sum_G, label="sum_G")
+PyPlot.legend()
+PyPlot.subplot(313)
+PyPlot.plot(sum_F, label="sum_F")
+PyPlot.legend()
+PyPlot.suptitle("Normalization Check (should be 1)")
+
+
+
+PyPlot.subplot(411)
+PyPlot.plot(avg_S, label="avg_S")
+PyPlot.legend()
+PyPlot.subplot(412)
+PyPlot.plot(avg_N, label="avg_N")
+PyPlot.legend()
+PyPlot.subplot(413)
+PyPlot.plot(avg_E, label="avg_E")
+PyPlot.legend()
+PyPlot.suptitle("Average of state variables")
+
+
+
+PyPlot.subplot(311)
+PyPlot.plot(Svalue,f, label="f Last")
+PyPlot.legend()
+PyPlot.subplot(312)
+PyPlot.plot(Nvalue,G, label="G last")
+PyPlot.legend()
+PyPlot.subplot(313)
+PyPlot.plot(Evalue,H, label="H Last")
+PyPlot.legend()
+PyPlot.suptitle("State variable distribution at final time step")
+
+
